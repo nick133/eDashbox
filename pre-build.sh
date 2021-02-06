17b3250efc06c90dc191207fe945040ec5bbe637
nick133 <nicksabaka@gmail.com>
2021-02-06 22:33:45 +0200

    Migration to Tup build system is completed

---
+++ pre-build.sh
@@ -0,0 +1,67 @@
+#!/bin/env bash
+#
+
+###
+##  Config
+#
+TUPOUT=Auto.tup
+PGM2C_DIR=Tools/pgm2c
+PGM2C=$PGM2C_DIR/bin/pgm2c
+SRC_TOPDIRS="Core Drivers FATFS Middlewares"
+UNUSED_FILES="Core/Src/freertos.c Makefile startup_stm32l432xx.s"
+BUILD_TARGETS="debug"
+#BUILD_TARGETS="release debug"
+
+# Preserve custom FatFs config from overwriting by CubeMX code generation
+OVERRIDE_SOURCES=FATFS/Target/ffconf.h
+
+MAIN_C=Core/Src/main.c
+if grep -q '^#include "cmsis_os.h"' ${MAIN_C}
+then
+    echo "==> Patching main.c to disable CMSIS-RTOS code"
+    sed -i \
+    -e 's@^#include "cmsis_os.h"@/* & */@;' \
+    -e 's@^ \+MX_FREERTOS_Init();@/* & */@' \
+    -e 's@^ \+osKernelStart();@/* & */@' \
+    -e 's@^ \+osKernelInitialize();@/* & */@' \
+    -e 's@^void MX_FREERTOS_Init(void);@/* & */@' ${MAIN_C}
+fi
+
+echo "==> Cleaning after CubeMX: $UNUSED_FILES"
+rm -f $UNUSED_FILES
+
+for to in ${OVERRIDE_SOURCES}
+do
+    from="`dirname $to`/override-`basename $to`"
+    if ! cmp -s $from $to
+    then
+        echo "==> Recovering $to after CubeMX"
+        cp -f $from $to
+    fi
+done
+
+echo "==> Building $TUPOUT"
+echo -e "# build.sh auto-generated TUP-file, do not edit!\n" > $TUPOUT
+
+find $SRC_TOPDIRS -type f -name "*.c" -printf 'srcs += %h/*.c\n' | sort -u >> $TUPOUT
+echo >> $TUPOUT
+find $SRC_TOPDIRS -type f -name "*.h" -printf 'incs += -I%h\n' | sort -u >> $TUPOUT
+
+if [[ "$PGM2C_DIR/src/pgm2c.nim" -nt "$PGM2C" ]]
+then
+    pushd $PGM2C_DIR
+    echo "==> Building pgm2c"
+    nimble build
+    popd
+fi
+
+for t in $BUILD_TARGETS
+do
+    if [[ ! -d "build-$t" ]]
+    then
+        echo -n "==> Creating build target: $t"
+        tup variant targets/${t}.tup
+    fi
+done
+
+#tup

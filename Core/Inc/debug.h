aaf46b0712ffc1374a41d5fa582a278778f94ffe
nick133 <nicksabaka@gmail.com>
2021-02-25 00:47:15 +0200

    float support macro for SEGGER_RTT_printf()

--- Core/Inc/debug.h
+++ Core/Inc/debug.h
@@ -3,13 +3,18 @@
 #define _DEBUG_H_
 
 # ifdef DEBUG
-
 #include "SEGGER_RTT.h"
 #include "SEGGER_RTT_Conf.h"
+#include <math.h>
 
-#define debug_printf(...)   SEGGER_RTT_printf(0, __VA_ARGS__)
+#define debug_printf(...)    SEGGER_RTT_printf(0, __VA_ARGS__)
 
-//extern uint8_t dbgflag;
+/* Fractional part of float to integer macro for SEGGER_RTT_printf()
+ * Usage: fftoi(1.238, 3) == 238
+ *        fftoi(1.238, 2) == 24
+ *        debug_printf("%u.%u", (int)1.238, fftoi(1.238, 2));
+ */
+#define fftoi(f, n)    (int)roundf((f - (int)f) * pow(10U, n))
 
 # else
 
